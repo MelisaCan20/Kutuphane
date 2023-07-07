20a15a51a5d70d5032c8955b924ffe1361a21208
MelisaCan20 <127137334+MelisaCan20@users.noreply.github.com>
2023-07-07 16:31:05 +0300

    EmanetIslemleri.Designer.cs

---
+++ EmanetIslemleri.Designer.cs
@@ -0,0 +1,372 @@
+namespace KutuphaneProje
+{
+    partial class EmanetIslemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmanetIslemleri));
+            this.sorgulaBtn = new System.Windows.Forms.Button();
+            this.teslimAlBtn = new System.Windows.Forms.Button();
+            this.emanetVerBtn = new System.Windows.Forms.Button();
+            this.kitapAdTxt = new System.Windows.Forms.TextBox();
+            this.kitapAdLbl = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.kutuphaneAdLbl = new System.Windows.Forms.Label();
+            this.personelAdLbl = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.uyeLbl = new System.Windows.Forms.Label();
+            this.araBtn1 = new System.Windows.Forms.Button();
+            this.uyeAdTxt = new System.Windows.Forms.TextBox();
+            this.araBtn2 = new System.Windows.Forms.Button();
+            this.uyeSoyadLbl = new System.Windows.Forms.Label();
+            this.uyeSoyadTxt = new System.Windows.Forms.TextBox();
+            this.kaydetBtn = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.emanetVerBilgi = new System.Windows.Forms.Label();
+            this.kaydetBtn2 = new System.Windows.Forms.Button();
+            this.kitapAdPnl = new System.Windows.Forms.Panel();
+            this.teslimAlBilgi = new System.Windows.Forms.Label();
+            this.sorgulaBilgi = new System.Windows.Forms.Label();
+            this.goruntuleBtn = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.kitapAdPnl.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sorgulaBtn
+            // 
+            this.sorgulaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sorgulaBtn.Location = new System.Drawing.Point(61, 303);
+            this.sorgulaBtn.Name = "sorgulaBtn";
+            this.sorgulaBtn.Size = new System.Drawing.Size(176, 72);
+            this.sorgulaBtn.TabIndex = 20;
+            this.sorgulaBtn.Text = "Emanet günü sorgula";
+            this.sorgulaBtn.UseVisualStyleBackColor = true;
+            this.sorgulaBtn.Click += new System.EventHandler(this.sorgulaBtn_Click);
+            // 
+            // teslimAlBtn
+            // 
+            this.teslimAlBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teslimAlBtn.Location = new System.Drawing.Point(61, 190);
+            this.teslimAlBtn.Name = "teslimAlBtn";
+            this.teslimAlBtn.Size = new System.Drawing.Size(176, 73);
+            this.teslimAlBtn.TabIndex = 19;
+            this.teslimAlBtn.Text = "Emaneti teslim al";
+            this.teslimAlBtn.UseVisualStyleBackColor = true;
+            this.teslimAlBtn.Click += new System.EventHandler(this.teslimAlBtn_Click);
+            // 
+            // emanetVerBtn
+            // 
+            this.emanetVerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emanetVerBtn.Location = new System.Drawing.Point(61, 74);
+            this.emanetVerBtn.Name = "emanetVerBtn";
+            this.emanetVerBtn.Size = new System.Drawing.Size(176, 72);
+            this.emanetVerBtn.TabIndex = 17;
+            this.emanetVerBtn.Text = "Emanet kitap ver";
+            this.emanetVerBtn.UseVisualStyleBackColor = true;
+            this.emanetVerBtn.Click += new System.EventHandler(this.emanetVerBtn_Click);
+            // 
+            // kitapAdTxt
+            // 
+            this.kitapAdTxt.Location = new System.Drawing.Point(93, 37);
+            this.kitapAdTxt.Multiline = true;
+            this.kitapAdTxt.Name = "kitapAdTxt";
+            this.kitapAdTxt.Size = new System.Drawing.Size(241, 20);
+            this.kitapAdTxt.TabIndex = 5;
+            // 
+            // kitapAdLbl
+            // 
+            this.kitapAdLbl.AllowDrop = true;
+            this.kitapAdLbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.kitapAdLbl.AutoSize = true;
+            this.kitapAdLbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kitapAdLbl.Location = new System.Drawing.Point(16, 44);
+            this.kitapAdLbl.Name = "kitapAdLbl";
+            this.kitapAdLbl.Size = new System.Drawing.Size(53, 13);
+            this.kitapAdLbl.TabIndex = 4;
+            this.kitapAdLbl.Text = "Kitap Ad :";
+            this.kitapAdLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.kitapAdLbl.UseMnemonic = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(571, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(521, 211);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // kutuphaneAdLbl
+            // 
+            this.kutuphaneAdLbl.AutoSize = true;
+            this.kutuphaneAdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kutuphaneAdLbl.Location = new System.Drawing.Point(1289, 785);
+            this.kutuphaneAdLbl.Name = "kutuphaneAdLbl";
+            this.kutuphaneAdLbl.Size = new System.Drawing.Size(93, 13);
+            this.kutuphaneAdLbl.TabIndex = 26;
+            this.kutuphaneAdLbl.Text = "... Kütüphanesi";
+            // 
+            // personelAdLbl
+            // 
+            this.personelAdLbl.AutoSize = true;
+            this.personelAdLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personelAdLbl.Location = new System.Drawing.Point(71, 785);
+            this.personelAdLbl.Name = "personelAdLbl";
+            this.personelAdLbl.Size = new System.Drawing.Size(64, 13);
+            this.personelAdLbl.TabIndex = 27;
+            this.personelAdLbl.Text = "Ad_Soyad";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 138);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(521, 211);
+            this.dataGridView2.TabIndex = 28;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // uyeLbl
+            // 
+            this.uyeLbl.AutoSize = true;
+            this.uyeLbl.Location = new System.Drawing.Point(33, 40);
+            this.uyeLbl.Name = "uyeLbl";
+            this.uyeLbl.Size = new System.Drawing.Size(54, 13);
+            this.uyeLbl.TabIndex = 29;
+            this.uyeLbl.Text = "Üye Ad  : ";
+            // 
+            // araBtn1
+            // 
+            this.araBtn1.Location = new System.Drawing.Point(411, 51);
+            this.araBtn1.Name = "araBtn1";
+            this.araBtn1.Size = new System.Drawing.Size(92, 42);
+            this.araBtn1.TabIndex = 17;
+            this.araBtn1.Text = "Ara";
+            this.araBtn1.UseVisualStyleBackColor = true;
+            this.araBtn1.Click += new System.EventHandler(this.araBtn1_Click);
+            // 
+            // uyeAdTxt
+            // 
+            this.uyeAdTxt.Location = new System.Drawing.Point(135, 33);
+            this.uyeAdTxt.Multiline = true;
+            this.uyeAdTxt.Name = "uyeAdTxt";
+            this.uyeAdTxt.Size = new System.Drawing.Size(241, 20);
+            this.uyeAdTxt.TabIndex = 17;
+            // 
+            // araBtn2
+            // 
+            this.araBtn2.Location = new System.Drawing.Point(363, 25);
+            this.araBtn2.Name = "araBtn2";
+            this.araBtn2.Size = new System.Drawing.Size(92, 42);
+            this.araBtn2.TabIndex = 30;
+            this.araBtn2.Text = "Ara";
+            this.araBtn2.UseVisualStyleBackColor = true;
+            this.araBtn2.Click += new System.EventHandler(this.araBtn2_Click);
+            // 
+            // uyeSoyadLbl
+            // 
+            this.uyeSoyadLbl.AutoSize = true;
+            this.uyeSoyadLbl.Location = new System.Drawing.Point(33, 85);
+            this.uyeSoyadLbl.Name = "uyeSoyadLbl";
+            this.uyeSoyadLbl.Size = new System.Drawing.Size(65, 13);
+            this.uyeSoyadLbl.TabIndex = 31;
+            this.uyeSoyadLbl.Text = "Üye Soyad :";
+            // 
+            // uyeSoyadTxt
+            // 
+            this.uyeSoyadTxt.Location = new System.Drawing.Point(135, 78);
+            this.uyeSoyadTxt.Multiline = true;
+            this.uyeSoyadTxt.Name = "uyeSoyadTxt";
+            this.uyeSoyadTxt.Size = new System.Drawing.Size(241, 20);
+            this.uyeSoyadTxt.TabIndex = 32;
+            // 
+            // kaydetBtn
+            // 
+            this.kaydetBtn.Location = new System.Drawing.Point(741, 364);
+            this.kaydetBtn.Name = "kaydetBtn";
+            this.kaydetBtn.Size = new System.Drawing.Size(184, 57);
+            this.kaydetBtn.TabIndex = 17;
+            this.kaydetBtn.Text = "Kaydet";
+            this.kaydetBtn.UseVisualStyleBackColor = true;
+            this.kaydetBtn.Visible = false;
+            this.kaydetBtn.Click += new System.EventHandler(this.kaydetBtn_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(54, 470);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(1013, 280);
+            this.dataGridView3.TabIndex = 33;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+            // 
+            // emanetVerBilgi
+            // 
+            this.emanetVerBilgi.AutoSize = true;
+            this.emanetVerBilgi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emanetVerBilgi.Location = new System.Drawing.Point(43, 473);
+            this.emanetVerBilgi.Name = "emanetVerBilgi";
+            this.emanetVerBilgi.Size = new System.Drawing.Size(230, 180);
+            this.emanetVerBilgi.TabIndex = 34;
+            this.emanetVerBilgi.Text = resources.GetString("emanetVerBilgi.Text");
+            this.emanetVerBilgi.Visible = false;
+            // 
+            // kaydetBtn2
+            // 
+            this.kaydetBtn2.Location = new System.Drawing.Point(761, 364);
+            this.kaydetBtn2.Name = "kaydetBtn2";
+            this.kaydetBtn2.Size = new System.Drawing.Size(148, 57);
+            this.kaydetBtn2.TabIndex = 35;
+            this.kaydetBtn2.Text = "Kaydet";
+            this.kaydetBtn2.UseVisualStyleBackColor = true;
+            this.kaydetBtn2.Visible = false;
+            this.kaydetBtn2.Click += new System.EventHandler(this.kaydetBtn2_Click);
+            // 
+            // kitapAdPnl
+            // 
+            this.kitapAdPnl.Controls.Add(this.kitapAdLbl);
+            this.kitapAdPnl.Controls.Add(this.kitapAdTxt);
+            this.kitapAdPnl.Controls.Add(this.araBtn2);
+            this.kitapAdPnl.Location = new System.Drawing.Point(591, 13);
+            this.kitapAdPnl.Name = "kitapAdPnl";
+            this.kitapAdPnl.Size = new System.Drawing.Size(476, 96);
+            this.kitapAdPnl.TabIndex = 36;
+            this.kitapAdPnl.Visible = false;
+            // 
+            // teslimAlBilgi
+            // 
+            this.teslimAlBilgi.AutoSize = true;
+            this.teslimAlBilgi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teslimAlBilgi.Location = new System.Drawing.Point(43, 473);
+            this.teslimAlBilgi.Name = "teslimAlBilgi";
+            this.teslimAlBilgi.Size = new System.Drawing.Size(266, 200);
+            this.teslimAlBilgi.TabIndex = 37;
+            this.teslimAlBilgi.Text = resources.GetString("teslimAlBilgi.Text");
+            this.teslimAlBilgi.Visible = false;
+            // 
+            // sorgulaBilgi
+            // 
+            this.sorgulaBilgi.AutoSize = true;
+            this.sorgulaBilgi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sorgulaBilgi.Location = new System.Drawing.Point(43, 473);
+            this.sorgulaBilgi.Name = "sorgulaBilgi";
+            this.sorgulaBilgi.Size = new System.Drawing.Size(257, 80);
+            this.sorgulaBilgi.TabIndex = 38;
+            this.sorgulaBilgi.Text = "Üye tablosundan aradığınız \r\nüyeyi seçtikten sonra \'Görüntüle\'ye \r\ntıklayarak üye" +
+    "ye ait emanet \r\nbilgilerini görüntüleyebilirsiniz. \r\n";
+            this.sorgulaBilgi.Visible = false;
+            // 
+            // goruntuleBtn
+            // 
+            this.goruntuleBtn.Location = new System.Drawing.Point(211, 364);
+            this.goruntuleBtn.Name = "goruntuleBtn";
+            this.goruntuleBtn.Size = new System.Drawing.Size(148, 57);
+            this.goruntuleBtn.TabIndex = 39;
+            this.goruntuleBtn.Text = "Görüntüle";
+            this.goruntuleBtn.UseVisualStyleBackColor = true;
+            this.goruntuleBtn.Visible = false;
+            this.goruntuleBtn.Click += new System.EventHandler(this.goruntuleBtn_Click);
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.dataGridView3);
+            this.panel.Controls.Add(this.goruntuleBtn);
+            this.panel.Controls.Add(this.uyeAdTxt);
+            this.panel.Controls.Add(this.kaydetBtn2);
+            this.panel.Controls.Add(this.kaydetBtn);
+            this.panel.Controls.Add(this.uyeLbl);
+            this.panel.Controls.Add(this.araBtn1);
+            this.panel.Controls.Add(this.kitapAdPnl);
+            this.panel.Controls.Add(this.uyeSoyadLbl);
+            this.panel.Controls.Add(this.uyeSoyadTxt);
+            this.panel.Controls.Add(this.dataGridView2);
+            this.panel.Controls.Add(this.dataGridView1);
+            this.panel.Location = new System.Drawing.Point(315, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1115, 765);
+            this.panel.TabIndex = 40;
+            // 
+            // EmanetIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(1435, 825);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.sorgulaBilgi);
+            this.Controls.Add(this.teslimAlBilgi);
+            this.Controls.Add(this.emanetVerBilgi);
+            this.Controls.Add(this.personelAdLbl);
+            this.Controls.Add(this.kutuphaneAdLbl);
+            this.Controls.Add(this.sorgulaBtn);
+            this.Controls.Add(this.teslimAlBtn);
+            this.Controls.Add(this.emanetVerBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EmanetIslemleri";
+            this.Text = "Emanet İşlemleri";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.kitapAdPnl.ResumeLayout(false);
+            this.kitapAdPnl.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sorgulaBtn;
+        private System.Windows.Forms.Button teslimAlBtn;
+        private System.Windows.Forms.Button emanetVerBtn;
+        private System.Windows.Forms.TextBox kitapAdTxt;
+        private System.Windows.Forms.Label kitapAdLbl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label kutuphaneAdLbl;
+        public System.Windows.Forms.Label personelAdLbl;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        public System.Windows.Forms.Label uyeLbl;
+        private System.Windows.Forms.Button araBtn1;
+        private System.Windows.Forms.TextBox uyeAdTxt;
+        private System.Windows.Forms.Button araBtn2;
+        public System.Windows.Forms.Label uyeSoyadLbl;
+        private System.Windows.Forms.TextBox uyeSoyadTxt;
+        private System.Windows.Forms.Button kaydetBtn;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label emanetVerBilgi;
+        private System.Windows.Forms.Button kaydetBtn2;
+        private System.Windows.Forms.Panel kitapAdPnl;
+        private System.Windows.Forms.Label teslimAlBilgi;
+        private System.Windows.Forms.Label sorgulaBilgi;
+        private System.Windows.Forms.Button goruntuleBtn;
+        private System.Windows.Forms.Panel panel;
+    }
+}
